4450a70b68c433c0fb6c7145014327ca4123122a
SAKET SINGH <saket742930@gmail.com>
2026-07-23 13:31:43 +0530

    Designed Voting System Menu

--- voting_system.py
+++ voting_system.py
@@ -1,1 +1,40 @@
-print("Hello World")
+candidates = [
+    "Candidate A",
+    "Candidate B",
+    "Candidate C"
+]
+
+votes = {
+    "Candidate A": 0,
+    "Candidate B": 0,
+    "Candidate C": 0
+}
+
+
+while True :
+    print("\n" + "=" * 50)
+    print(" " * 17 + "VOTING SYSTEM")
+    print("=" * 50 + "\n")
+
+    print("1. View Candidates")
+    print("2. Cast Vote")
+    print("3. View Results")
+    print("4. Exit")
+
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1 :
+        print("View Candidates Selected")
+        
+    elif choice == 2 :
+        print("Cast Vote Selected")
+        
+    elif choice == 3 :
+        print("View Results Selected")
+        
+    elif choice == 4 :
+        print("Exiting ... ")
+        break
+        
+    else :
+        print("Invalid Input !")
